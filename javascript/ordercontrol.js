815c39a44498cbfed7ebdff875c77bca5f8acaf2
Mustafa <officewalamustafa@gmail.com>
2020-05-23 21:36:59 +0530

    orderpage styling

--- javascript/ordercontrol.js
+++ javascript/ordercontrol.js
@@ -7,6 +7,12 @@ let items = [["Pav Bhaji","Biryani","Chole Bhature","Samosa"],
 		 ["Pasta Macroli","Tortano","Pizza Margherita","Pizza Cappriciossa"],
 		 ["Fried rice","Noodles","Manchurian","Kebab"],
 		 ["Gulab Jamun","Rasagulla","Cheese Cake","Hot chocolate cake"]];
+let price = [[100,150,120,45],
+			 [70,50,50,50],
+			 [70,110,130,150],
+			 [120,90,60,130],
+			 [130,120,140,150]];
+let pricel = [];
 function a(l,i){
 	totalv++;
 	let o = document.getElementById("cartvalue");
@@ -16,20 +22,13 @@ function a(l,i){
 		if(list[j]==items[l][i]){flag=1;break;}
 	}
 	if(flag==1)listv[j]++;
-	else {list[list.length] = items[l][i];listv[listv.length]=1;}
-}
-
-function val(){
-	let x="";
-	for(i=0;i<list.length;i++){
-		x += list[i] +"" + listv[i]+"</br>";
-	}
-	return x;
+	else {list[list.length] = items[l][i];listv[listv.length]=1;pricel[pricel.length]=price[l][i];}
 }
 
 function cartwindow(){
 	sessionStorage.items = JSON.stringify(items);
 	sessionStorage.list = JSON.stringify(list);
 	sessionStorage.listv = JSON.stringify(listv);
+	sessionStorage.price = JSON.stringify(pricel);
 	window.location.href = 'order.html';
 }
